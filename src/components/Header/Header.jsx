f7ab5ec80b202cfce0c13b499816a5942021f30e
Martin Sevcik <prixedox@gmail.com>
2024-06-16 18:42:12 +0200

    updated menu to container

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -89,7 +89,7 @@ const Header = () => {
 
   return (
     <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary custom-navbar">
-      <div className="container-fluid">
+      <div className="container">
         <div className="nav-item">
           <a className="nav-link custom-brand" href="#home">
             Home
@@ -107,7 +107,7 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link custom-link" href="#education">
                 Education
